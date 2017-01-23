0668e62cceb38e624240c254485e2382265d004d
Mike <miguel.rojas@bairesdev.com>
2017-01-23 15:20:14 -0300

    Added comments and cleaned up functions

--- modal/public/javascripts/ca-modal.js
+++ modal/public/javascripts/ca-modal.js
@@ -1,7 +1,12 @@
 (function () {
     'use strict';
 
-    // fade out
+    var modalTriggers = document.querySelectorAll('[data-modal]');
+
+    /**
+     * [fadeOut function to animate and hide a given element]
+     * @param  {[String]} el [The matching element that will fade out]
+     */
     function fadeOut(el){
       el.style.opacity = 1;
 
@@ -18,7 +23,10 @@
       })();
     }
 
-    // fade in
+    /**
+     * [fadeIn function to animate and show a given element]
+     * @param  {[String]} el [The matching element that will fade in]
+     */
     function fadeIn(el, display){
 
       if (el.classList.contains('is-hidden')){
@@ -30,7 +38,7 @@
       (function fade() {
 
         var val = parseFloat(el.style.opacity);
-        
+
         if (!((val += .1) > 1)) {
           el.style.opacity = val;
           requestAnimationFrame(fade);
@@ -38,10 +46,14 @@
       })();
     }
 
-    function createModal(el){
+    /**
+     * [createModal Creates modal markup, attaches it to the DOM showing it with a fade in animation]
+     */
+    function createModal(){
 
       var caModalCloseBtn = '<a class="ca-modal__close" data-modal="close"></a>';
       var caModal = document.createElement("div");
+      var el;
 
       caModal.setAttribute("class", "ca-modal");
       document.body.appendChild(caModal);
@@ -50,6 +62,9 @@
       fadeIn(el);
     }
 
+    /**
+     * [modalSwitch Attaches a click event listener to the document and handles the modal show/hide ]
+     */
     function modalSwitch() {
       document.onclick = function(event) {
 
@@ -58,18 +73,18 @@
 
         if ( modalTriggerState === 'open' ) {
           if (!el) {
-            console.log('Modal instance doesnt exists');
-            createModal(el);
+            createModal();
           } else {
             fadeIn(el);
           }
-        } else if (document.querySelectorAll("[data-modal]")[2].className === 'ca-modal__close' ) {
+        } else if ( modalTriggerState === 'close' ) {
           fadeOut(el);
-          console.log(el);
-          console.log("Closing");
         }
       }
     }
 
-    modalSwitch();
+    if( modalTriggers.length > 0 ) {
+      modalSwitch();
+    }
+
 })();
